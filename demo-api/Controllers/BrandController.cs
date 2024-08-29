d62ed7b09ea5fa49c14f5af8c9c8ab3feefe0ea6
NhanNguyen8080 <boiboii162122@gmail.com>
2024-08-29 09:58:30 +0700

    update code

---
+++ demo-api/Controllers/BrandController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        [HttpGet]
+        [Route("list-all")]
+        public async Task<IActionResult> ListAllAsync()
+        {
+            return Ok("Hello");
+        }
+    }
+}
